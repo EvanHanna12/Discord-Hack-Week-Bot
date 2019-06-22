7dc0344257d40c74cc7fac7d8e404851c5cc33a6
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-06-21 21:46:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ bot.on("guildDelete", guild => {
    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
    bot.user.setActivity(`Serving ${bot.guilds.size} servers | w!help`);
 });
+bot.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "welcome-leave").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+bot.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
 
 bot.on("message", async message => {
     if(message.author.bot) return;
