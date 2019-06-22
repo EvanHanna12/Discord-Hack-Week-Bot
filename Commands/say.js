c1fb51cbf3c78c6e5f0718b3cf491b67776e5517
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-06-21 19:51:43 -0500

    Create say.js

---
+++ Commands/say.js
@@ -0,0 +1,34 @@
+const errors = require("../../utils/errors.js");
+
+exports.run = async (client, message, args) => {
+  if(!args [0] || args[0] == "help"){
+   message.reply("Usage: !say <#channel> <word or sentence>");
+   return;
+  }
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  
+  
+  message.delete();
+  let botmessage;
+  let channel = message.mentions.channels.first()
+  if(channel) {
+    botmessage = args.slice(1).join("")
+    channel.send(botmessage)
+  } else {
+   botmessage = args.join(" ");
+   message.channel.send(botmessage)
+  }
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['s'],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'say',
+    description: 'Gives you a random response to a question.',
+    usage: '8ball [question]'
+};
