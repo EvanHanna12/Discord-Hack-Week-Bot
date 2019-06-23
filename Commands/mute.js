577ca45718638aa23cf8e6fa60afc58f97bddc64
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-06-23 15:56:04 -0500

    Update mute.js

--- Commands/mute.js
+++ Commands/mute.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply("Usage: !tomute <@user> <#s/h/d/m>");
+  if(!tomute) return message.reply("Usage: w!mute <@user> <#s/h/d/m>");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
   let muterole = message.guild.roles.find(`name`, "Muted");
   if(!muterole){
