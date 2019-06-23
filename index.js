300107114ced410bdc0a611ec1f18cd88e459fd8
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-06-23 16:11:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,39 +12,13 @@ bot.on("ready", async() => {
     bot.user.setActivity(`With MasterCoder#0001`)
 });
 
-bot.on("guildCreate", guild => {
-    console.log(`I have been added to: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    bot.user.setActivity(`Serving ${bot.guilds.size} servers | w!help`);
-  });
-  
-bot.on("guildDelete", guild => {
-   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-   bot.user.setActivity(`Serving ${bot.guilds.size} servers | w!help`);
-});
-bot.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-  newUsers[guild.id].set(member.id, member.user);
-
-  if (newUsers[guild.id].size > 10) {
-    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find(channel => channel.name === "welcome-leave").send("Welcome our new users!\n" + userlist);
-    newUsers[guild.id].clear();
-  }
-});
-
-bot.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-});
-
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") message.reply("Dm's are not allowed sorry")
     if(message.content.startsWith('r/')) return;
   
 
-    bot.user.setActivity(`Prefix ${prefix}`)
+    bot.user.setActivity(`With Code`)
     if(!message.content.startsWith(prefix)){
         console.log("      " + message.author.username + '#' + message.author.discriminator + ' | ' + message.content)
         
